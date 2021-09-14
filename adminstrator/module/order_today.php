b5581282e1b8a83f54b29a24ce1322085fad3e1d
awaheedp <a.waheed@gmail.com>
2021-09-14 11:39:01 +0530

    orginal

--- adminstrator/module/order_today.php
+++ adminstrator/module/order_today.php
@@ -69,14 +69,12 @@ if (count($order_program_arr) > 0) {
               //  print_r();
                 $arr             = $day_controller->get_address_where_program_id($order_program_arr[$i]['user_area_id']);
                 $driver_name     = $controller->get_driver_name_where_area_id($order_program_arr[$i]['area_id']);
-                // echo "driver_name $driver_name <br/>";
-                echo '<pre>'; print_r($driver_name); echo '</pre>';
                 $user_name       = $controller->get_user_info_where_id($order_program_arr[$i]['user_area_id']);
                 $area_name = ($_SESSION['lang'] == "en") ? $order_program_arr[$i]['area_name_eng'] : $order_program_arr[$i]['area_name_ar'];
 
                 echo '<tr><td class="sorting_1" >' . $user_name["name"] . '</td>
                                      <td class="sorting_1" >' . $user_name["telep"] . '</td>
-                                      <td class="sorting_1" >' . $area_name. '</td>
+                                      <td class="sorting_1" >' . $driver_name["name"]. '</td>
                                       <td class="sorting_1" >' . $area_name. '</td>
                                       <td class="sorting_1" >' . $arr['user_area_block'] . '</td>
                                       <td class="sorting_1" >' . $arr['user_area_street'] . '</td>
